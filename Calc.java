4f6a30730ed264174fb607a90a646c18774c5d13
POUYANFAR AMIRHOSSEIN <pouyanfar@heckel02.u-pem.fr>
2023-09-28 15:16:46 +0200

    format function changed to println

--- Calc.java
+++ Calc.java
@@ -8,16 +8,16 @@ public class Calc {
     // Show a request
     System.out.println("Enter a number: ");
 
-    int value_1 = scanner.nextInt(); // variable of type integer
-    int value_2 = scanner.nextInt(); // Same
+    int value_1 = scanner.nextInt(); // integer variable
+    int value_2 = scanner.nextInt(); // Same thing
 
 
 
     System.out.println(value_1 + value_2); // We are going to print:
-    System.out.println(value_1 - value_2); // the substraction,
+    System.out.println(value_1 - value_2); // the difference,
     System.out.println(value_1 * value_2); // the product,
     System.out.println(value_1 / value_2); // the quotient and
-    System.out.println(value_1 % value_2); // the le reste.
+    System.out.println(value_1 % value_2); // the divide remaining.
 
   }
 }
